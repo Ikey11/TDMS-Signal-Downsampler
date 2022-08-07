ec70be6b6c681f11da36a3ab0725f9ae40568c4d
Mason <maysgaming00@gmail.com>
2022-08-07 02:05:06 -0700

    Optimised downsample function. Low-pass butterworth filter TBD

--- TDMS Signal Downsampler/TDMS_Signal_Downsampler.py
+++ TDMS Signal Downsampler/TDMS_Signal_Downsampler.py
@@ -15,31 +15,45 @@ Mason Becker
 import numpy as np  # Array processing
 import scipy as sy  # Various signal processing tools
 import scipy.signal as sig
+import time
 
 # Additional Functions
 import plotlib as PL
 import DataManager as DM
 
+# Low-pass butterworth filter
+def butter_lowpass_filter(data, cutoff, fs, order=5):
+    nyq = 0.5 * fs
+    normal_cutoff = cutoff / nyq
+    b, a = butter(order, normal_cutoff, btype='low', analog=False)
+    y = lfilter(b, a, data)
+    return y
+
 # Takes every n value out of a data set and creates a new reduced value
 def downsample(data, interval):
-    c = 0  # Counter
     reduced = np.empty([int(data.shape[0]), int(data.shape[1]/interval)])
     for channel in range(0, data.shape[0]):
         index = 0  # Reduced data datum counter
-        for datum in range(0, data.shape[1]):
-            c += 1
-            if c == interval:
-                reduced[channel][index]=data[channel][datum]
-                index += 1
-                c = 0
+        for datum in range(0, data.shape[1], interval):
+            reduced[channel][index]=data[channel][datum]
+            index += 1
 
     return reduced
 
 # Main
 if __name__ == "__main__":
 
-    # UI
     print("TDMS Signal Downsampler\nBy Mason Becker\n\n")
+
+    # Data reading
+    file = DM.get_filepaths(r"Input")
+
+    print("Input:")
+    for doc in file:
+        print(doc)
+    print("Total files: " + str(len(file)) + "\n")
+
+    # UI
     while True:
         try:
             # Custom output parameters
@@ -63,12 +77,9 @@ if __name__ == "__main__":
         except:
             print("Invalid input")
             continue
-    
-
-    # Data reading
-    file = DM.get_filepaths(r"Input")
 
     # Iterates between each file
+    starttime = time.time()
     first = True
     for doc in file:
         # Retreves tdms data
@@ -89,9 +100,8 @@ if __name__ == "__main__":
             matrix = np.append(matrix, reduce_data, 1)
             raw_matrix = np.append(raw_matrix, data, 1)
 
-    # Preform anti-aliasing process
-
     # Data analysis
+    print("Time Spent: " + str(time.time() - starttime) + "s")
     print("Final matrix shape: " + str(matrix.shape))
     print("Raw:")
     print(raw_matrix)
@@ -102,9 +112,6 @@ if __name__ == "__main__":
     if outConfig < 3:
         DM.mat_save(matrix)
     if outConfig > 1:
-        print("Channel: " + str(channel))
-        for i in range(0, len(matrix[channel])):
-            print(str(i) + ": " + str(matrix[channel][i]))
         # Sets limits or undoes erroneous limits
         if lim1 == lim2 == 0 or lim1 < 0 or lim2 > raw_matrix.shape[1] or lim1 > lim2:
             lim1 = 0
