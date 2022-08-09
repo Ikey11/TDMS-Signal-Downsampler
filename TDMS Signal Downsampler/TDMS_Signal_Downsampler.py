c5a35bedcbe1cc8299dd96250c63d6c75fcb4348
Mason <maysgaming00@gmail.com>
2022-08-08 20:54:55 -0700

    Added stripped down low-pass filter [NONFUNCTIONAL]

--- TDMS Signal Downsampler/TDMS_Signal_Downsampler.py
+++ TDMS Signal Downsampler/TDMS_Signal_Downsampler.py
@@ -21,21 +21,23 @@ import time
 import plotlib as PL
 import DataManager as DM
 
-# Low-pass butterworth filter
-def butter_lowpass_filter(data, cutoff, fs, order=5):
-    nyq = 0.5 * fs
-    normal_cutoff = cutoff / nyq
-    b, a = butter(order, normal_cutoff, btype='low', analog=False)
-    y = lfilter(b, a, data)
+# Low-pass filter
+def lowpass_filter(data, interval, order=8):
+    system = sig.dlti(*sig.cheby1(order, 0.05, 0.8 / interval))
+    b, a = system.num, system.den
+    y = sig.filtfilt(b, a, data, axis=-1)
     return y
 
 # Takes every n value out of a data set and creates a new reduced value
-def downsample(data, interval):
-    reduced = np.empty([int(data.shape[0]), int(data.shape[1]/interval)])
-    for channel in range(0, data.shape[0]):
+def downsample(data, interval, order=8):
+    print("Passing lowpass filter...")
+    noiseless = lowpass_filter(data, interval, order)
+    print("Downsampling...")
+    reduced = np.empty([int(noiseless.shape[0]), int(noiseless.shape[1]/interval)])
+    for channel in range(0, noiseless.shape[0]):
         index = 0  # Reduced data datum counter
-        for datum in range(0, data.shape[1], interval):
-            reduced[channel][index]=data[channel][datum]
+        for datum in range(0, noiseless.shape[1], interval):
+            reduced[channel][index]=noiseless[channel][datum]
             index += 1
 
     return reduced
@@ -87,7 +89,7 @@ if __name__ == "__main__":
         print(doc + " shape: " + str(data.shape))
 
         # Proforms individual decimation process for data handling
-        reduce_data = downsample(data, 1000)
+        reduce_data = downsample(data, 10)
         
         print(doc + " decimated shape: " + str(reduce_data.shape))
 
