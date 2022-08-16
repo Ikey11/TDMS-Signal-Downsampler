fbf36feb1fec547790260a07c3d1bbf90244d729
Mason <maysgaming00@gmail.com>
2022-08-15 17:19:48 -0700

    Bound lowpass filter to prime factorizations derived from the decimation factor. Removed limits option.

--- TDMS Signal Downsampler/TDMS_Signal_Downsampler.py
+++ TDMS Signal Downsampler/TDMS_Signal_Downsampler.py
@@ -16,28 +16,54 @@ import numpy as np  # Array processing
 import scipy as sy  # Various signal processing tools
 import scipy.signal as sig
 import time
+import math
 
 # Additional Functions
 import plotlib as PL
 import DataManager as DM
 
+# Attempts to produce a unique factorization for input integer
+def factorize(n):
+    factors = []
+    # even case
+    while n % 2 == 0:
+        factors.append(2)
+        n = n / 2
+
+    # odd case
+    for i in range(3, int(math.sqrt(n)) + 1, 2):
+        while(n % i == 0):
+            factors.append(i)
+            n = n / i
+    
+    # Catch remainder
+    if n > 2:
+        factors.append(n)
+
+    return factors
+
 # Low-pass filter
-def lowpass_filter(data, interval, order=8):
-    system = sig.dlti(*sig.cheby1(order, 0.05, 0.8))
+def lowpass_filter(data, interval, factors, order=8):
+    system = sig.dlti(*sig.cheby1(order, 0.05, 0.8 / factors[0]))
     b, a = system.num, system.den
     y = sig.filtfilt(b, a, data, axis=-1)
+
+    # Preform recursion in accordence with interval factors
+    if len(factors) > 1:
+        lowpass_filter(y, interval, factors = factors[1:], order=order) # Slice a factor off and run recursion
     return y
 
 # Takes every n value out of a data set and creates a new reduced value
 def downsample(data, interval, order=8):
     print("Passing lowpass filter...")
-    noiseless = lowpass_filter(data, interval, order)
+    noiseless = lowpass_filter(data, interval, factors = factorize(interval), order=order)
     print("Downsampling...")
-    reduced = np.empty([int(noiseless.shape[0]), int(noiseless.shape[1]/interval)])
+    reduced = np.empty([int(noiseless.shape[0]), math.ceil(noiseless.shape[1]/interval)])
     for channel in range(0, noiseless.shape[0]):
         index = 0  # Reduced data datum counter
         for datum in range(0, noiseless.shape[1], interval):
             reduced[channel][index]=noiseless[channel][datum]
+            # TBD: Conserve remaining data
             index += 1
 
     return reduced
@@ -45,7 +71,7 @@ def downsample(data, interval, order=8):
 # Main
 if __name__ == "__main__":
 
-    print("TDMS Signal Downsampler\nBy Mason Becker\n\n")
+    print("TDMS Signal Downsampler\nBy Mason Becker\n")
 
     # Data reading
     file = DM.get_filepaths(r"Input")
@@ -61,20 +87,11 @@ if __name__ == "__main__":
             # Custom output parameters
             print("Decimate factor:")
             intervalConfig = int(input())
-            print("Output:\n1. Mat file [NONFUNCTIONAL]\n2. Mat file and interactable figure\n3. Interactable figure")
+            print("Output:\n1. Mat file\n2. Mat file and interactable figure\n3. Interactable figure")
             outConfig = int(input())
             if outConfig > 3 or outConfig < 1:
                 raise Exception
             if outConfig > 1:
-                print("Set custom limit for graph analysis?\n1. Yes\n2. No")
-                c = input()
-                if c == '1':
-                    print("Set lim1:")
-                    lim1 = int(input())
-                    print("Set lim2:")
-                    lim2 = int(input())
-                else:
-                    lim1 = lim2 = 0
                 print("Set channel:")
                 channel = int(input())
             break
@@ -84,6 +101,7 @@ if __name__ == "__main__":
 
     # Iterates between each file
     starttime = time.time()
+    print(factorize(intervalConfig))
     first = True
     for doc in file:
         # Retreves tdms data
@@ -93,7 +111,7 @@ if __name__ == "__main__":
         # Proforms individual decimation process for data handling
         #reduce_data = lowpass_filter(data, intervalConfig)
         reduce_data = downsample(data, intervalConfig)
-        
+
         print(doc + " decimated shape: " + str(reduce_data.shape))
 
         # Create data matrix
@@ -117,9 +135,6 @@ if __name__ == "__main__":
     if outConfig < 3:
         DM.mat_save(matrix)
     if outConfig > 1:
-        # Sets limits or undoes erroneous limits
-        if lim1 == lim2 == 0 or lim1 < 0 or lim2 > raw_matrix.shape[1] or lim1 > lim2:
-            lim1 = 0
-            lim2 = raw_matrix.shape[1]
-        #PL.interactable_compairson(raw_matrix, matrix, channel, lim1, lim2, intervalConfig)
+        lim1 = 0
+        lim2 = raw_matrix.shape[1]
         PL.direct_compare(raw_matrix, matrix, channel, lim1, lim2, intervalConfig)
