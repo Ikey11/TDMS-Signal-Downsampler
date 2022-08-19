0690e030bad31a1f2a9b2f6e01525b0882151d6b
Mason <maysgaming00@gmail.com>
2022-08-19 12:36:43 -0700

    Implimented sig.decimate command Removed raw_matrix variable

--- TDMS Signal Downsampler/TDMS_Signal_Downsampler.py
+++ TDMS Signal Downsampler/TDMS_Signal_Downsampler.py
@@ -13,7 +13,7 @@ Mason Becker
 
 # Imports
 from numpy import empty, append  # Array processing
-from scipy.signal import cheby1, dlti, filtfilt  # Low-pass filter mechanics
+from scipy.signal import cheby1, dlti, filtfilt, decimate  # Low-pass filter mechanics
 from time import time  # Compilation time
 from math import sqrt, ceil
 
@@ -42,20 +42,20 @@ def factorize(n):
     return factors
 
 # Low-pass filter
-def lowpass_filter(data, interval, factors, order=8):
+def lowpass_filter(data, factors, order=8):
     system = dlti(*cheby1(order, 0.05, 0.8 / factors[0]))
     b, a = system.num, system.den
     y = filtfilt(b, a, data, axis=-1)
 
     # Preform recursion in accordence with interval factors
     if len(factors) > 1:
-        lowpass_filter(y, interval, factors = factors[1:], order=order) # Slice a factor off and run recursion
+        y = lowpass_filter(y, factors = factors[1:], order=order) # Slice a factor off and run recursion
     return y
 
 # Takes every n value out of a data set and creates a new reduced value
 def downsample(data, interval, order=8):
     print("Passing lowpass filter...")
-    noiseless = lowpass_filter(data, interval, factors = factorize(interval), order=order)
+    noiseless = lowpass_filter(data, factors = factorize(interval), order=order)
     print("Downsampling...")
     reduced = empty([int(noiseless.shape[0]), ceil(noiseless.shape[1]/interval)])
     for channel in range(0, noiseless.shape[0]):
@@ -65,6 +65,16 @@ def downsample(data, interval, order=8):
             # TBD: Conserve remaining data
             index += 1
 
+    return reduced
+
+def sig_decimate(data, factors, order=8):
+    print("Decimating...")
+    reduced = decimate(data, factors[0], order, ftype='iir')
+    print(reduced.shape)
+
+    # Preform recursion in accordence with interval factors
+    if len(factors) > 1:
+        reduced = sig_decimate(reduced, factors = factors[1:], order=order) # Slice a factor off and run recursion
     return reduced
 
 # Main
@@ -101,39 +111,39 @@ if __name__ == "__main__":
     # Iterates between each file
     starttime = time()
     print(factorize(intervalConfig))
-    first = True
+    num = 1
     for doc in file:
+        print("File (" + str(num) + "/" + str(len(file)) + ")")
         # Retreves tdms data
         data = DM.tdms_read(doc)
         print(doc + " shape: " + str(data.shape))
 
         # Proforms individual decimation process for data handling
         #reduce_data = lowpass_filter(data, intervalConfig)
-        reduce_data = downsample(data, intervalConfig)
+        #reduce_data = downsample(data, intervalConfig)
+        reduce_data = sig_decimate(data, factorize(intervalConfig))
 
         print(doc + " decimated shape: " + str(reduce_data.shape))
 
         # Create data matrix
-        if first:
+        if num == 1:
             matrix = reduce_data
-            raw_matrix = data
+            #raw_matrix = data
             first = False
         else:
             matrix = append(matrix, reduce_data, 1)
-            raw_matrix = append(raw_matrix, data, 1)
+            #raw_matrix = append(raw_matrix, data, 1)
+        num += 1
 
     # Data analysis
     print("Time Spent: " + str(time() - starttime) + "s")
     print("Final matrix shape: " + str(matrix.shape))
-    print("Raw:")
-    print(raw_matrix)
-    print("Reduced:")
-    print(matrix)
 
     # Data export
     if outConfig < 3:
         DM.mat_save(matrix)
     if outConfig > 1:
-        lim1 = 0
-        lim2 = raw_matrix.shape[1]
-        PL.direct_compare(raw_matrix, matrix, channel, lim1, lim2, intervalConfig)
+        #lim1 = 0
+        #lim2 = raw_matrix.shape[1]
+        #PL.direct_compare(raw_matrix, matrix, channel, lim1, lim2, intervalConfig)
+        PL.channel_plot(matrix, channel)
